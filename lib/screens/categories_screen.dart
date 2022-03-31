23dbaae8691452d666a8b2b851e894239dfa829f
Chlopi <chlopi@Alinas-MacBook-Pro.local>
2022-03-31 13:52:03 +0200

    Creating Navigation between Categories and CategoriesMeal screens

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meal_aplication/models/category_model.dart';
+import 'package:flutter_meal_aplication/screens/category_meals_screen.dart';
 import 'package:flutter_meal_aplication/widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -10,28 +11,36 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     Size screenSize = MediaQuery.of(context).size;
     TextTheme textTheme = Theme.of(context).textTheme;
+
+    void selectCategory(BuildContext context) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => const CategoryMealsScreen(), fullscreenDialog: true));
+    }
 
     return Scaffold(
       appBar: AppBar(title: Text('Categories', style: textTheme.titleLarge),),
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(top: 20),
-          width: screenSize.width * 0.9,
-          height: screenSize.height,
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 500, // width
-              mainAxisExtent: 200, // height
-              childAspectRatio: 2/3,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
+        child: GestureDetector(
+          onTap: () => selectCategory(context),
+          child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            width: screenSize.width * 0.9,
+            height: screenSize.height,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 500, // width
+                mainAxisExtent: 200, // height
+                childAspectRatio: 2/3,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+              ),
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return CategoryItem(title: categories[index].title, backgroundColor: categories[index].color);
+              },
             ),
-            itemCount: categories.length,
-            itemBuilder: (context, index) {
-              return CategoryItem(title: categories[index].title, backgroundColor: categories[index].color);
-            },
           ),
         ),
       ),
